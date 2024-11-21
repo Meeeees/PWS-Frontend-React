50aee9a21a2b17d38575937e1343c5c966a84212
meeeeesgithub <119116083+meeeeesgithub@users.noreply.github.com>
2024-11-21 17:41:21 +0100

    veranderingen

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import WebcamCapture from "./webcam";
 import config from "./config.json";
 import Button from "@mui/material/Button";
 import styles from "./styles/app.module.css";
-import { Box, Modal, Typography } from "@mui/material";
+import { Box, Input, Modal, Typography } from "@mui/material";
 function App() {
   const [imgSrc, setImgSrc] = useState(null);
   const [Status, setStatus] = useState(null);
@@ -53,11 +53,13 @@ function App() {
               onClose={handleClose}
               aria-labelledby="modal-modal-title"
               aria-describedby="modal-modal-description"
+              style={{ padding: "20px" }}
             >
               <Box
                 sx={{
                   position: 'absolute',
                   top: '50%',
+                  padding: "20px",
                   left: '50%',
                   transform: 'translate(-50%, -50%)',
                   width: "60vw",
@@ -71,21 +73,21 @@ function App() {
                 <Typography id="modal-title" variant="h6" component="h2">
                   Add New People
                 </Typography>
-                <Typography id="modal-description" sx={{ mt: 2 }}>
-                  <form action={`${config.Backend}/add`} method="post">
-                    <input type="text" name="name" placeholder="name" />
-                    Tevreden met de video?<button type="submit">Voeg toe</button>
-                  </form>
-                  {ModalIsOpen && <img src={`${config.Backend}/video`} alt="live feed" />}
-                  <button onClick={() => {
-                    fetch(config.Backend + "/start").then(async res => {
-                      setStatus(5)
-                      startTimer()
-                    })
-                  }}>Start video</button>
-                  <p>{Status}</p>
-                  <p>Neem een video waarbij je het complete gezicht in meerdere hoeken laat zien</p>
-                </Typography>
+                {/* <Typography id="modal-description" sx={{ mt: 2 }}> */}
+                <form action={`${config.Backend}/add`} method="post">
+                  <Input variant="outlined" type="text" name="name" placeholder="name" />
+                  Tevreden met de video?<Button variant="outlined" type="submit">Voeg toe</Button>
+                </form>
+                {ModalIsOpen && <img src={`${config.Backend}/video`} alt="live feed" />}
+                <Button variant="outlined" onClick={() => {
+                  fetch(config.Backend + "/start").then(async res => {
+                    setStatus(5)
+                    startTimer()
+                  })
+                }}>Start video</Button>
+                <p>{Status}</p>
+                <p>Neem een video waarbij je het complete gezicht in meerdere hoeken laat zien</p>
+                {/* </Typography> */}
               </Box>
             </Modal>
 
